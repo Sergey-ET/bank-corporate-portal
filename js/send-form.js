dfbbd0fc0347d9c8cd76af9654321d5f4f234b24
Sergey Troshkin <sergey.troshkin@gmail.com>
2023-01-27 22:22:33 +0300

    Add server data upload

--- js/send-form.js
+++ js/send-form.js
@@ -5,17 +5,28 @@ form.addEventListener('submit', (e) => {
 
   let output = {};
   new FormData( form ).forEach(( value, key ) => {
-    // Check if property already exist
     if ( Object.prototype.hasOwnProperty.call( output, key ) ) {
       let current = output[ key ];
       if ( !Array.isArray( current ) ) {
-        // If it not an array, convert it to an array.
         current = output[ key ] = [ current ];
+        current.push( value );
       }
-      current.push( value ); // Add the new value to the array.
     } else {
       output[ key ] = value;
     }
   });
+
+  fetch('transform.php', {
+    method: 'POST',
+    body: JSON.stringify(output),
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8'
+    }
+  })
+  .then((response) => {
+    console.log(response);
+  });
+
   console.log(JSON.stringify(output));
+  e.target.reset();
 });
